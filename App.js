a20a0f11a2d89d2d0eece96d541b9ec5faaa4f41
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-01-17 10:59:26 -0300

    feat: add Tab bar and RecentExpenses and AllExpenses screens

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import RecentExpensesScreen from "./screens/RecentExpensesScreen";
+import { Button, StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AllExpensesScreen from "./screens/AllExpensesScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#280191" },
+            headerTintColor: "#e7e7e7",
+            tabBarStyle: { backgroundColor: "#280191" },
+            tabBarActiveTintColor: "#e7e7e7",
+            tabBarActiveBackgroundColor: "#4201f3",
+            tabBarInactiveTintColor: "#afafaf",
+            headerRight: ({ tintColor }) => (
+              <Ionicons
+                name="add"
+                color={tintColor}
+                size={24}
+                style={styles.headerButton}
+              />
+            ),
+          }}
+        >
+          <Tab.Screen
+            name="RecentExpenses"
+            component={RecentExpensesScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="hourglass" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="AllExpenses"
+            component={AllExpensesScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="calendar" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootContainer: { backgroundColor: "#240677" },
+  headerButton: {
+    marginRight: 24,
+    marginVertical: "auto",
   },
 });
---
+++ screens/AllExpensesScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function AllExpensesScreen() {
+  return <Text>AllExpensesScreen</Text>;
+}
